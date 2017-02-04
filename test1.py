df54a202f994ca9233b29d56073b7adc96ac91da
shubham <shubhamchauhan1004@gmail.com>
2017-02-04 18:24:37 +0530

    Adding selenium module

--- test1.py
+++ test1.py
@@ -1,9 +1,14 @@
+#import urllib
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://hindimoviesonline.watch/dangal-full-movie-online/'
+url = 'http://www.kumby.com/avatar-the-last-airbender-book-3-chapter-5/'
 
 source_object = requests.get(url)
-soup = BeautifulSoup(source_object.content, 'html.parser')
-#tag = soup.findall('video', {'class':'jw-video jw-reset'})
-print(soup.prettify())
+soup = BeautifulSoup(source_object.text, 'html.parser')
+k = soup.prettify()
+#print(k)
+important = soup.findAll("embed","object","param","video")
+print(len(important))
+for i in important:
+	print(i)
